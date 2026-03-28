7f10cf362f0d52b047f50e3883838a14cdd93e71
John Gorriceta <johngorriceta1@gmail.com>
2026-03-28 13:16:49 -0600

    fixed images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-   vite: {
+  output: 'static',
+  vite: {
     plugins: [tailwindcss()],
   },
-  // add yur domain name here
-  site: 'https://lexingtonthemes.com',
+  site: 'https://userabbithole.com',
   compressHTML: true,
   integrations: [sitemap()]
 });
